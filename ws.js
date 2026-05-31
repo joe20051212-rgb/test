9b0f654d25d8bb0fdd555c90091c7d8b8672c718
joe20051212-rgb <joe.2005.12.12@gmail.com>
2026-06-01 00:55:51 +0200

    updates to log-ing and errors

--- ws.js
+++ ws.js
@@ -5,8 +5,21 @@ console.log('[log]:{S}:(01)'); // 01 means server started
 
 wss.on('connection', (ws) => {
   console.log('[log]:{S}:(+)'); // new user
-//  ws.send(''); // not used
+  ws.send('+');
+  
+  ws.on('message', (data) => {
+    console.log('[log]:{M}:', data); // new messege
+  });
+  
+  ws.on('error', (error) => {
+    console.error('[log]:{E}:', error); // client and other error
+  });
+  
   ws.on('close', () => {
     console.log('[log]:{S}:(-)'); // lost user
   });
+});
+
+wss.on('error', (error) => {
+  console.error('[log]:{S.E}:', error); // server errors get there own line
 });
